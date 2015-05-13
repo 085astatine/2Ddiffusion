c3b01de0c0eff82a6262166c6e74a7f82e017149
085astatine <astatine085@gmail.com>
2015-05-13 13:37:17 +0900

    CMakeListsを作成
    OpenGL, GLUT, Eigen3のfind_packageを行う
    FindEigen3.cmakeはEigen3のcmakeからcopyした

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <Eigen/Core>
+#include <GL/glut.h>
+
+void init(void){
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+void display(void){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+}
+
+int main(int argc, char *argv[]){
+    std::cout << "2D Diffusion" << std::endl;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA);
+    glutCreateWindow(argv[0]);
+    glutDisplayFunc(display);
+    init();
+    glutMainLoop();
+    return 0;
+}
